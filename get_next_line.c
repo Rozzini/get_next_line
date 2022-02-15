f2402329fa63f2dff7ffa506988be7f7a597186b
Maksim Rasporskyi <mraspors@lab2r3s12.42abudhabi.ae>
2022-02-16 03:55:50 +0400

    need to add strjoin

--- get_next_line.c
+++ get_next_line.c
@@ -12,37 +12,116 @@
 
 #include "get_next_line.h"
 
-//read(fd, &s[i], BUFFER_SIZE)
+char	*ft_rewrite(char *result, char *s)
+{
+	char	*temp;
+	size_t	len_tmp;
+	size_t	len_s;
+
+
+	if (result == NULL)
+	{
+		len_s = ft_strlen(s);
+		result = (char *)malloc(sizeof(char) * (len_s));
+		ft_strlcpy(result, s, len_s);
+		free(s);
+	}
+	else
+	{
+		len_tmp = ft_strlen(result);
+		temp = (char *)malloc(sizeof(char) * len_tmp);
+		ft_strlcpy(temp, result, len_tmp);
+		free(result);
+		len_s = ft_strlen(s);
+		result = (char *)malloc(sizeof(char) * (len_s + len_tmp));
+		ft_strlcpy(result, temp, len_tmp);
+		ft_strlcpy(&result[len_tmp], s, len_s);
+		free(s);
+	}
+	return (result);
+}
+
+char	*save(char *buf, char *result, char *s, char *p)
+{
+	char	*temp;
+	size_t	len_temp;
+	size_t	len_sp;
+
+	len_temp = ft_strlen(p);
+	buf = (char *)malloc(sizeof(char) * len_temp);
+	ft_strlcpy(buf, p, len_temp);
+	p[0] = '\0';
+	result = ft_rewrite(result, s);
+	return (result);
+}
 
 char	*get_next_line(int fd)
 {
-	char	*result_s;
-	char	s[50000];
-	int		i;
+	static char	*buf;
+	char		*result;
+	char		*s;
+	char		*p;
+	size_t		i;
+	size_t		len;
 
-	i = 0;
-	result_s = NULL;
-	if (BUFFER_SIZE < 1 || fd == -1)
-		return (NULL);
-	while (read(fd, &s[i], BUFFER_SIZE) != 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	result = NULL;
+	printf("\nBuf: %s\n", buf);
+	if (buf != NULL)
 	{
-		if (s[i] == '\n')
-			break;
-		i++;
+		i = ft_strlen(buf);
+		s = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+		ft_strlcpy(s, buf, i);
+		free(buf);
+		p = ft_strchr(s, BUFFER_SIZE);
+		if (p == NULL)
+		{
+			result = (char *)malloc(sizeof(char) * i);
+			ft_strlcpy(result, s, i + 1);
+			free(s);
+		}
+		else
+		{
+			save(buf, result, s, p);
+			return (result);
+		}
 	}
-	s[i + 1] = '\0';
-	result_s = (char *)malloc(ft_strlen(s) * sizeof(char));
-	ft_strlcpy(result_s, s, ft_strlen(s) - 1);
-	return (result_s);
+	s = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	s[BUFFER_SIZE + 1] = '\0';
+	while (read(fd, s, BUFFER_SIZE) != 0)
+	{
+		p = ft_strchr(s, BUFFER_SIZE);
+		if (p != NULL)
+			break;
+		else
+			result = ft_rewrite(result, s);
+		s = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+		s[BUFFER_SIZE + 1] = '\0';	
+	}
+	save(buf, result, s, p);
+	return (result);
 }
 
-/*int main ()
+//printf("\n\ncheckpoint 1\n\n");
+
+int main ()
 {
     	int fd;
 	char *s;
 
 	fd = open("test.txt", O_RDONLY);
+
+	printf("\noutput1\n");
 	s = get_next_line(fd);
-	printf("string: %s.\n", s);
+	printf("%s_\n", s);
+
+	printf("\noutput2\n");
+	s = get_next_line(fd);
+	printf("%s_\n", s);
+
+	printf("\noutput3\n");
+	s = get_next_line(fd);
+	printf("%s_\n", s);
 	return 0;
-}*/
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -28,11 +28,16 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 	size_t	src_len;
 
+	if (dstsize == 0)
+	{
+		dst = NULL;
+		return (0);
+	}
 	i = 0;
 	src_len = (size_t)ft_strlen(src);
 	if (dstsize > 0)
 	{
-		while (i < dstsize - 1)
+		while (i < dstsize)
 		{
 			if (src[i] != '\0')
 				dst[i] = src[i];
@@ -45,20 +50,18 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	return (src_len);
 }
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int buf_size)
 {
 	char	*str;
 	int		i;
 
 	i = 0;
 	str = (char *)s;
-	while (str[i] != '\0')
+	while (i < buf_size)
 	{
-		if (str[i] == (char)c)
-			return (&str[i]);
+		if (str[i] == '\n' || str[i] == '\0')
+			return (&str[i + 1]);
 		i++;
 	}
-	if (c == '\0')
-		return (&str[i]);
 	return (NULL);
 }
